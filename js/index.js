57804d0b829cac63989c77c17c60aa309c21df3d
seb99seb <sebvangpedersen@hotmail.dk>
2022-10-26 12:35:25 +0200

    op til opgave 3 lavet

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+document.getElementById("additem").addEventListener("click",AddUnhealthyItem)
+
+let elementInput = document.getElementById('newGoods')
+
+function AddUnhealthyItem(e){
+    let inputText = (elementInput).value
+    console.log(inputText)
+    let listUnhealthy = document.getElementById("listUnhealthy")
+    let newLi = document.createElement("li")
+    let newText = document.createTextNode(inputText)
+    newLi.appendChild(newText)
+    newLi.setAttribute("class", "unhealthy")
+
+    listUnhealthy.appendChild(newLi)
+}
